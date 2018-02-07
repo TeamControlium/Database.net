fc8b36383fa8e0f03683208980c15bc4f02d0ab3
Mat Walker <mat@alexview.com>
2018-02-07 15:58:59 +1000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Team Controlium Database")]
+[assembly: AssemblyDescription("Database interfacing helpers primarily for use within the Team Controlium suite and associated frameworks")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Team Controlium contributors")]
+[assembly: AssemblyProduct("Database.net")]
+[assembly: AssemblyCopyright("Copyright 2018 - Mat Walker and the Team Controlium contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec4b1a4e-1368-48fe-ab3c-7c40f82a8a4e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: NeutralResourcesLanguage("en-AU")]
